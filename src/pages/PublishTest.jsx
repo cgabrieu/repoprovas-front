ce5969cfd4df8ab5e0674e516097dedaf7cbd671
Carlos Gabriel <carlosgabrielpc@live.com>
2021-12-18 16:39:54 -0300

    feat: add get and post course api methods

--- src/pages/PublishTest.jsx
+++ src/pages/PublishTest.jsx
@@ -1,20 +1,34 @@
-import React, {  } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import PageContainer from '../components/PageContainer';
 
 export default function PublishTest() {
+  const [coursesList, setCoursesList] = useState(null);
+
+  useEffect(() => {
+    
+  }, []);
 
   return (
     <PageContainer>
       <TitleContainer>
         <TitleText>Qual Curso?</TitleText>
-        <AddSomethingButton>
-          Não encontrou? Adicione aqui
-        </AddSomethingButton>
+        <AddSomethingButton>Não encontrou? Adicione aqui</AddSomethingButton>
+        {!coursesList.length ? (
+          <h1> Ainda não existem cursos registrados! </h1>
+        ) : (
+          coursesList.map((course) => (
+            <ItemContainer>{course.name}</ItemContainer>
+          ))
+        )}
       </TitleContainer>
     </PageContainer>
   );
 }
+
+const ItemContainer = styled.div`
+  font-size: 26px;
+`;
 
 const AddSomethingButton = styled.div`
   position: absolute;
@@ -22,19 +36,26 @@ const AddSomethingButton = styled.div`
   top: -2px;
   padding: 5px;
   font-size: 20px;
-  background-color: #FFFAEF;
+  background-color: #fffaef;
   border-radius: 5px;
+  text-align: center;
   cursor: pointer;
-  
+  @media (max-width: 700px) {
+    position: relative;
+    width: 220px;
+    margin: 10px 0;
+  }
 `;
 
 const TitleContainer = styled.div`
   position: relative;
-  border: 1px red solid;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const TitleText = styled.h1`
   font-size: 26px;
   text-align: center;
-  color: #FFFAEF;
+  color: #fffaef;
 `;
--- src/services/api/api.js
+++ src/services/api/api.js
@@ -2,45 +2,18 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://mywallet-cgabrieu.herokuapp.com"
+    process.env.NODE_ENV === "prod"
+      ? "https://repoprovas-cgabrieu.herokuapp.com"
       : "http://localhost:4000",
 });
 
-function getConfig(token) {
-  return {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
+
+export function getCoursesList() {
+  return api.get("/courses");
 }
 
-export function postSignIn(email, password) {
-  return api.post("/auth/sign-in", {
-    email,
-    password,
+export function postCourse(name) {
+  return api.post("/courses", {
+    name,
   });
-}
-
-export function postSignUp(inputFields) {
-  return api.post("/auth/sign-up", inputFields);
-}
-
-export function getTransactions(token) {
-  return api.get("/transactions", getConfig(token));
-}
-
-export function postNewTransaction(value, description, token) {
-  return api.post(
-    "/transactions",
-    {
-      description,
-      value,
-    },
-    getConfig(token)
-  );
-}
-
-export function deleteTransaction(token, transactionId) {
-  return api.delete(`/transactions/${transactionId}`, getConfig(token));
 }
